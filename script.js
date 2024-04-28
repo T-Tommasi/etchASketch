4771acb56bbc6c6fac4bdc5e44692fbf8a728909
T-Tommasi <ttommasi1945@gmail.com>
2024-04-28 12:50:12 +0200

    (feat) created starting grid layout
    Created container div with flexbox
    Added logic for generating grid based on user-input
    Added basic styling to grid tiles

---
+++ script.js
@@ -0,0 +1,17 @@
+//Global variables used in the project
+const CONTAINER = document.querySelector('.container');
+let times = prompt('How many squares per side?', '16')
+let flex_basis = 100/times
+
+
+//Set up a 16*16 grid with the tiles
+function createGrid (times) {
+    for (i = 0; i < times*times; ++i) {
+        let grid_tile = document.createElement('div');
+        grid_tile.setAttribute('class','tile')
+        grid_tile.style.flexBasis = `0% 0% ${flex_basis}%`
+        CONTAINER.appendChild(grid_tile)
+    }
+}
+
+createGrid(times)

e0d1848c2b5cd1b999732066eb1b23b1cb81e6bf
T-Tommasi <ttommasi1945@gmail.com>
2024-04-30 17:37:06 +0200

    (fix) fixed grid overflow problems and logic
    Added a menu for selecting rainbow, black, or restarting the grid
    Fixed problems wit flexbox not stacking correctly
    TODO: fix the grid still giving problems by not shrinking

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Global variables used in the project
-const CONTAINER = document.querySelector('.container');
+const CONTAINER = document.querySelector('.grid');
 let times = prompt('How many squares per side?', '16')
 let flex_basis = 100/times
 
@@ -9,7 +9,7 @@ function createGrid (times) {
     for (i = 0; i < times*times; ++i) {
         let grid_tile = document.createElement('div');
         grid_tile.setAttribute('class','tile')
-        grid_tile.style.flexBasis = `0% 0% ${flex_basis}%`
+        grid_tile.style.flexBasis = `${flex_basis}%`
         CONTAINER.appendChild(grid_tile)
     }
 }

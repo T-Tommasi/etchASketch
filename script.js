32d41825be09839a6f3f4953f8c43a786a6a9864
T-Tommasi <ttommasi1945@gmail.com>
2024-05-02 12:55:21 +0200

    (feat) Added RESET function
    Created variable for resetting the grill size from user input
    Added function for cleaning the previous grill and rewrite anew
    Refractored code for increased readability

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 //Global variables used in the project
 const CONTAINER = document.querySelector('.grid');
-let times = prompt('How many squares per side?', '16');
-let flex_basis = 100/times;
+const RESET_BUTTON = document.querySelector('#reset');
+const BLACK_BUTTON = document.querySelector('#black');
 
+//set a function for determining the number of squares
+let squaresPerSide = () => prompt('How many suqares per side?', '16')
 
 //Set up a 16*16 grid with the tiles
 function createGrid (times) {
+    let flex_basis = 100/times;
     for (i = 0; i < times*times; ++i) {
         let grid_tile = document.createElement('div');
-        grid_tile.setAttribute('class','tile')
-        grid_tile.style.flexBasis = `${flex_basis}%`
-        CONTAINER.appendChild(grid_tile)
+        grid_tile.setAttribute('class','tile');
+        grid_tile.style.flexBasis = `${flex_basis}%`;
+        CONTAINER.appendChild(grid_tile);
     }
 }
 
-createGrid(times)
+//Function that transform the squares background to black if hovered
+
+//Initial function for populating the page
+createGrid(squaresPerSide())
+
+//Set up event listeners for the various buttons
+//RESET button should reset the grill to the specified square size
+RESET_BUTTON.addEventListener('click', function () {
+    CONTAINER.innerHTML = '';
+    createGrid(squaresPerSide());
+})
